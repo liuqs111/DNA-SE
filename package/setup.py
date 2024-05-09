3b979b9dc906474fdb2fba170738fe1e4354d732
liuqs111 <95181546+liuqs111@users.noreply.github.com>
2024-05-09 14:45:13 +0800

    Update setup.py

--- package/setup.py
+++ package/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="DNA_SE", # Replace with your own username
-    version="0.0.3",
+    version="0.0.4",
     author="Qinshuo",
     author_email="u3008680@connect.hku.hk",
     description="A small example package",
