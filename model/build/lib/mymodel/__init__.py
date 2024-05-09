a1c8bd3ae4b804bc8187e9ebb188e9c0900a94e4
liuqs111 <95181546+liuqs111@users.noreply.github.com>
2024-05-09 14:48:03 +0800

    Delete model directory

--- model/build/lib/mymodel/__init__.py
+++
@@ -1,8 +0,0 @@
-# package
-# __init__.py
-import re
-import urllib
-import sys
-import os
-
-__all__=["model_b_training"] # 列表可以根据要导入的模块数而进行新增，列表元素是之前新建的py文件的名字
--- model/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import  setup
-setup(
-      name = 'model_b_training.py',
-      version = '0.1',
-      author = 'qinshuo',
-      py_modules=['mymodel.model_b_training'] 
-)
