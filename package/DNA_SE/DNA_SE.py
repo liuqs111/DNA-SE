5ef501e6f020a0d24d6a7a386a804b10c1b45d1d
liuqs111 <95181546+liuqs111@users.noreply.github.com>
2024-05-24 14:37:41 +0800

    Update DNA_SE.py

--- package/DNA_SE/DNA_SE.py
+++ package/DNA_SE/DNA_SE.py
@@ -3,9 +3,11 @@ from torch import nn
 from torch.optim import Adam
 import torch.utils.data as Data
 import numpy as np
-import torch.nn.functional as F     
+import torch.nn.functional as F    
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation 
 
-def dnase(p, Oi, BATCHSIZE, EPOCH, M, B, input_dim, hidden_dim, output_dim, K_func, C_func, psi_func, depth, activation = 'tanh', device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')):
+def dna(p, Oi, BATCHSIZE, EPOCH, M, B, input_dim, hidden_dim, output_dim, K_func, C_func, depth, psi_func = None, esti_mask = False, activation = 'tanh', device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')):
     
     '''
     p: int, the number of features X
@@ -43,20 +45,19 @@ def dnase(p, Oi, BATCHSIZE, EPOCH, M, B, input_dim, hidden_dim, output_dim, K_fu
             for i in range(depth):
                 if i == 0:
                     layers.append(nn.Linear(input_dim, hidden_dim))
+                    if activation == 'relu':
+                        layers.append(nn.ReLU())
+                    elif activation == 'sigmoid':
+                        layers.append(nn.Sigmoid())
+                    elif activation == 'tanh':
+                        layers.append(nn.Tanh())
+                    else:
+                        raise ValueError("Unsupported activation function.")
                 elif i == depth - 1:
                     layers.append(nn.Linear(hidden_dim, output_dim))
                 else:
                     layers.append(nn.Linear(hidden_dim, hidden_dim))
-                    
-                if activation == 'relu':
-                    layers.append(nn.ReLU())
-                elif activation == 'sigmoid':
-                    layers.append(nn.Sigmoid())
-                elif activation == 'tanh':
-                    layers.append(nn.Tanh())
-                else:
-                    raise ValueError("Unsupported activation function.")
-                    
+                     
             self.layers = nn.ModuleList(layers)
             
         def forward(self, x):
@@ -90,25 +91,29 @@ def dnase(p, Oi, BATCHSIZE, EPOCH, M, B, input_dim, hidden_dim, output_dim, K_fu
     optimizer_b = torch.optim.Adam(Parameters_b, lr=1e-3)
     Parameters_beta = beta.parameters()
     optimizer_beta = torch.optim.Adam(Parameters_beta, lr=1e-2)
+    losses = []
+    epochs = []
     
     for epoch in range(EPOCH):
         for step, (t_train, _) in enumerate(train_loader):
             
-            s = torch.linspace(min(Oi),max(Oi),M).reshape(M,1).to(device)
-            t = t_train[..., -1]  
+            t = torch.linspace(int(min(Oi)),int(max(Oi)),M).reshape(M,1).to(device)
+            s = t_train[..., -1].reshape(BATCHSIZE,1).to(device)
+            ones_M = torch.ones(M,1).to(device)
 
             def loss_function_integral(s, t, Oi, beta, model_b):
                 
-                inter_left = torch.sum(K_func(s,t,Oi,beta)*model_b(s,Oi))
-                inter_right = C_func(t,Oi,beta) + model_b(t,Oi)
-                return torch.mean(torch.sum((inter_left - inter_right)**2))
+                inter_left = torch.mean(K_func(s,t,Oi,beta)*torch.kron(ones_M,model_b(s)).reshape(M,BATCHSIZE), dim=1).reshape(M,1)
+                inter_right = C_func(t,Oi,beta) + model_b(t)
+                return torch.mean((inter_left - inter_right)**2)
             
             optimizer_b.zero_grad()
             loss_integral = loss_function_integral(s, t, Oi, beta, model_b)
             loss_integral.backward()
             optimizer_b.step()
+
             
-            if step % 5 == 0:
+            if step % 5 == 0 and esti_mask == 1:
                 
                 def loss_function_estimate(Oi, beta, model_b):
                 
@@ -119,9 +124,38 @@ def dnase(p, Oi, BATCHSIZE, EPOCH, M, B, input_dim, hidden_dim, output_dim, K_fu
                 loss_estimate.backward()
                 optimizer_beta.step()
 
-        if epoch % 100 == 0:
+        if epoch % 100 == 0 and esti_mask == 1:
             print(f'Epoch {epoch}: loss = {loss_estimate.item()}')
-    return model_b, beta.data(), optimizer_b, optimizer_beta
+        if epoch % 1 == 0 and esti_mask == 0:
+            print(f'Epoch {epoch}: loss = {loss_integral.item()}')
+            losses.append(loss_integral.item())
+            epochs.append(epoch)
+
+    if esti_mask == 1:
+        return model_b, beta.data()
+    else:
+        return model_b
+
+# a simple example
+# p = 1
+# Oi = torch.rand(1000,1)
+# BATCHSIZE = 100
+# EPOCH = 200
+# M = 1000
+# B = 10000
+# input_dim = 1
+# output_dim = 1
+# hidden_dim = 5
+# depth = 2
+# ones_B = torch.ones(M,1).to(torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+
+# def K_func(x,y,Oi,b):
+#     return y * torch.kron(ones_B,x).reshape(BATCHSIZE,M)
+# def C_func(x,Oi,b):
+#     return -torch.exp(x)
+# output = dna(p, Oi, BATCHSIZE, EPOCH, M, B, input_dim, hidden_dim, output_dim, K_func, C_func, depth)
+# print(output)
+
 '''
 beta.data() is used to get the value of beta
 '''
